f8e6df8f46f6da3ece21e59365354a11db0ebd6f
TeeL18 <torrence_95@hotmail.com>
2025-01-12 21:38:58 +0000

    Added JS to my project

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
+//London
+setInterval(function () {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
 
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
+}, 1000);
+
+//Amsterdam
+setInterval(function () {
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
+
+  amsterdamDateElement.innerHTML = amsterdamTime.format("MMMM Do YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "h:mm:ss [<small>]A[</small]"
+  );
+}, 1000);
